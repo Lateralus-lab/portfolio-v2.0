ce1db1eef7a06f31696c80c6067ec3a45f3ef860
Lateralus <zoofoo.online@gmail.com>
2020-10-20 23:53:28 +0100

    rebuild better version of navmenu & fixed bugs

--- assets/js/components/navMenu.js
+++ assets/js/components/navMenu.js
@@ -3,27 +3,15 @@ burger.addEventListener('click', navMenu);
 function navMenu(e) {
     e.preventDefault();
 
-    // Open nav menu on burger click
+    // Menu on burger click
     this.classList.toggle('open');
-    navList.classList.toggle('open');
-    nav.style.display = 'block';
-    nav.style.zIndex = '100';
-
-    // Close mav menu on link click
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            if (navList.classList.contains('open')) {
-                this.classList.remove('open');
-                navList.classList.remove('open');
-                nav.style.display = 'none';
-                nav.style.zIndex = '-100';
-            }
-        });
-    });
-
-    // Close nav menu on burger click
-    if (!navList.classList.contains('open')) {
-        nav.style.display = 'none';
-        nav.style.zIndex = '-100';
-    }
+    nav.classList.toggle('open');
 }
+
+// Close menu on link click
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        burger.classList.remove('open');
+        nav.classList.remove('open');
+    });
+});
